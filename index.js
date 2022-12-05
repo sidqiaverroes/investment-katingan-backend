48221d4e0a2783246404ae5fa3c20ded13975224
sidqiaverroes <sidqi.averroes26@gmail.com>
2022-12-05 15:14:21 +0700

    fix: modify cors options

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 //routes
 app.use("/api/users", userRoute);
